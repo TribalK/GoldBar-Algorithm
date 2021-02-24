727525ac8bab01fb1be16252b34a8104cbe66f39
Derek Drackley <derekdrackley@gmail.com>
2021-02-24 01:45:42 -0600

    Finished project implementation

--- src/test/GoldBar.java
+++ src/test/GoldBar.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.openqa.selenium.WebElement;
+import java.util.ArrayList;
 
 //GoldBar class - object-oriented class for determine validity of all gold bars
 public class GoldBar {
@@ -25,6 +26,17 @@ public class GoldBar {
 		return number_of_bars;
 	}
 	
+	public int insertFind() {
+		for(int j=0; j<number_of_bars; j++) {
+			if(!fakeBar[j] && !visited[j]) {
+				visited[j] = true;
+
+				return j;
+			}
+		}
+		return -1;
+	}
+	
 	//Check how many bars are still undetermined
 	public boolean identifyFakeBar() {
 		int numFake = 0;
@@ -34,24 +46,67 @@ public class GoldBar {
 				numFake++;
 		}
 		
-		return numFake >= 1;
+		return numFake > 1;
+	}
+	
+	public int revealFakeBar() {
+		for(int i=0; i<number_of_bars; i++)
+		{
+			if(!fakeBar[i])
+				return i;
+		}
+		
+		return -1;
 	}
 	
 	//Checking sign of "Weighings" output for each number.
-	public String checkSign(WebElement test) {
+	public int checkSign(WebElement test, ArrayList<Integer> leftBowl, ArrayList<Integer> rightBowl, ArrayList<Integer> extraBowl) {
 		
-		if(test.getText().equals("<"))
-			return "LESS THAN!";
+		int real_count = 0;
 		
-		else if(test.getText().equals(">"))
-			return "GREATER THAN!";
+		if(test.getText().equals("<")) {
+			setRealBars(rightBowl);
+			setRealBars(extraBowl);
+			real_count = rightBowl.size()+extraBowl.size();
+		}
+			
+		else if(test.getText().equals(">")) {
+			setRealBars(leftBowl);
+			setRealBars(extraBowl);
+			real_count = rightBowl.size()+extraBowl.size();
+		}
 		
-		else if(test.getText().equals("="))
-			return "EQUAL!";
+		else if(test.getText().equals("=")) {
+			setRealBars(leftBowl);
+			setRealBars(rightBowl);
+			real_count = rightBowl.size()+extraBowl.size();
+		}
 		
-		else
-			return "Nothing worked.";
+		else {
+			System.out.println("Nothing worked.");
+		}
+		
+		return real_count;
 	}
+	
+	public void setRealBars(ArrayList<Integer> bowl) {
+		for(int i=0; i<bowl.size(); i++)
+		{
+			int realBarVal = bowl.get(i);
+			
+			fakeBar[realBarVal] = true;
+		}
+	}
+
+	public void resetFakeVisited() {
+		for(int i=0; i<visited.length; i++)
+		{
+			if(!fakeBar[i])
+				visited[i] = false;
+		}
+	}
+	
+	
 	
 	
 	
--- src/test/GoldBarTest.java
+++ src/test/GoldBarTest.java
@@ -1,4 +1,4 @@
-/********************************************************************************
+/************************************************************************************
  * Author: Derek Drackley
  * Project: GoldBarTest.java
  * 
@@ -8,11 +8,15 @@
  * Using Selenium to open the webpage and simulate inputs to produce outputs in the
  * most efficent manner.
  * 
- * Execution Method: javac -d ../../GoldBarTest.java in src/test/ folders,
- * then running java test.GoldBarTest in parent directories
+ * Ensure that the selenium server jar file is loaded into classpath
+ * Version ran for project: selenium-server-standalone-4.0.0-alpha-2.jar
  * 
- * Status: In-Progress
- ********************************************************************************/
+ * Execution Method: Navigate to /src/test/ folders in location where you have
+ * files stored. Run "javac -d ../../ GoldBarTest.java GoldBar.java" in command line,
+ * then go back two parent directories and run "java test.GoldBarTest"
+ * so ChromeDriver.exe can be recognized from driver directory
+ *
+ ***********************************************************************************/
 
 package test;
 
@@ -22,7 +26,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+//Importing non-Selenium packages
 import java.util.Scanner;
+import java.util.ArrayList;
 
 //Driver class
 public class GoldBarTest {
@@ -32,8 +39,6 @@ public class GoldBarTest {
 		WebDriver driver;
 		Scanner input = new Scanner(System.in);
 		
-		//*[@id='root']/div/div[1]/div[5]/ol/li[1]  - CORRECT USAGE for xpath!
-		
 		//Get user's current directory (when tested by other engineer)
 		String projectLocation = System.getProperty("user.dir");
 		
@@ -42,12 +47,9 @@ public class GoldBarTest {
 
 		try
 		{
-			//int count = 1;
-			int size = gold.getSize();
-			
-			//TODO: Place while loop to determine when we find the true fake gold bar
-			//Function is implemented in GoldBar.java but will not be set here until prepared
-			
+			int real_bars = 0;
+			int number_of_bars = gold.getSize();
+
 			//Establish the System to be using the Chrome WebDriver for displaying the web page
 			System.setProperty("webdriver.chrome.driver", projectLocation+"\\driver\\chromedriver.exe");
 			driver = new ChromeDriver();
@@ -58,57 +60,71 @@ public class GoldBarTest {
 			//The WebDriver object will navigate to that link in a new Chrome browser
 			driver.get(baseURL);
 			
-			//Currently test examples to ensure numeric input and button functionality works successfully
-			driver.findElement(By.id("left_0")).sendKeys("4");
-			driver.findElement(By.id("left_1")).sendKeys("5");
-			driver.findElement(By.id("left_2")).sendKeys("6");
-			driver.findElement(By.id("left_3")).sendKeys("7");
-			driver.findElement(By.id("left_4")).sendKeys("8");
-			driver.findElement(By.id("right_0")).sendKeys("0");
-			driver.findElement(By.id("right_1")).sendKeys("1");
-			driver.findElement(By.id("right_2")).sendKeys("2");
-			driver.findElement(By.id("right_3")).sendKeys("3");
-			driver.findElement(By.id("weigh")).click();
+			while(gold.identifyFakeBar())
+			{
 			
-			//May switch to using data structure to hold each new entry
-			WebElement test = driver.findElement(By.id("reset"));
+				int containerSize = (number_of_bars - real_bars)/3;
+				ArrayList<Integer> leftBowl = new ArrayList<Integer>();
+				ArrayList<Integer> rightBowl = new ArrayList<Integer>();
+				ArrayList<Integer> extraBowl = new ArrayList<Integer>();
+				
+				for(int i=0; i<containerSize; i++) {
+					int unvisitedNum = gold.insertFind();
+					
+					if(unvisitedNum != -1) {
+						driver.findElement(By.id("left_"+i)).sendKeys(Integer.toString(unvisitedNum));
+						leftBowl.add(unvisitedNum);
+					}
+				}
+				
+				for(int i=0; i<containerSize; i++) {
+					int unvisitedNum = gold.insertFind();
+					
+					if(unvisitedNum != -1) {
+						driver.findElement(By.id("right_"+i)).sendKeys(Integer.toString(unvisitedNum));
+						rightBowl.add(unvisitedNum);
+					}
+				}
+				
+				for(int i=containerSize*2; i<number_of_bars; i++)
+				{
+					int unvisitedNum = gold.insertFind();
+					
+					if(unvisitedNum != -1)
+						extraBowl.add(unvisitedNum);
+					
+				}
+				//Currently test examples to ensure numeric input and button functionality works successfully
 			
-			//Testing output
-			System.out.printf("The sign found is: %s %n",test.getText());
+				driver.findElement(By.id("weigh")).click();
 			
-			//Check if sign is currently valid (will be used for determining which numbers to utilize next
-			System.out.println(gold.checkSign(test));
+				//May switch to using data structure to hold each new entry
+				WebElement test = driver.findElement(By.id("reset"));
+				
+				//Testing output
+				System.out.printf("The sign found is: %s %n",test.getText());
+				
+				//Check if sign is currently valid (will be used for determining which numbers to utilize next
+				int curr_real = gold.checkSign(test, leftBowl, rightBowl, extraBowl);
+				
+				real_bars += curr_real;
+				
+				//Click reset button to flush inputs
+				//Had difficulties initially setting the right find element technique due to the other reset ID
+				//found when Inspecting, it constantly conflicted
+				driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div[4]/button[1]")).click();
+				
+				gold.resetFakeVisited();
+
+			}
 			
-			//Click reset button to flush inputs
-			//Had difficulties initially setting the right find element technique due to the other reset ID
-			//found when Inspecting, it constantly conflicted
-			driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div[4]/button[1]")).click();
-			
-			driver.findElement(By.id("left_0")).sendKeys("0");
-			driver.findElement(By.id("left_1")).sendKeys("1");
-			driver.findElement(By.id("right_0")).sendKeys("2");
-			driver.findElement(By.id("right_1")).sendKeys("3");
-			driver.findElement(By.id("weigh")).click();
-			
-			//Using xpath version of findElement to determine relative path for each new weighings.
-			//May switch to using data structure to hold each new entry
-			test = driver.findElement(By.id("reset"));
-			
-			//Testing output
-			System.out.printf("The sign found is: %s %n",test.getText());
-			
-			//Check if sign is currently valid (will be used for determining which numbers to utilize next
-			System.out.println(gold.checkSign(test));
-			
-			//Click reset button to flush inputs
-			//Had difficulties initially setting the right find element technique due to the other reset ID
-			//found when Inspecting, it constantly conflicted
-			driver.findElement(By.xpath("//*[@id='root']/div/div[1]/div[4]/button[1]")).click();
-			
+			int fakeBarIndex = gold.revealFakeBar();
+			//Click on box associated with the fake gold bar
+			driver.findElement(By.id("coin_"+fakeBarIndex)).click();
 			
 			//Checking for reset button functionality without closing the webpage immediately
 			System.out.println("Enter any character to proceed.");
-			char testInput = input.next().charAt(0);
+			input.next().charAt(0);
 			
 			//Close all windows of the new browser (vs. using driver.close())
 			//Good to exit out of driver object to prevent possible memory leaks
